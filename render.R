6808dbdc3b9ddb3c103bb00d6a5844ed4a8de5b4
Matt Shotwell <biostatmatt@gmail.com>
2022-01-14 18:48:09 -0600

    initial commit

---
+++ render.R
@@ -0,0 +1,15 @@
+## This R script is executed as a cron job
+
+## Get command line arguments
+args <- commandArgs(trailingOnly=TRUE)
+
+## Run report and output document
+tryCatch(
+  rmarkdown::render(
+    input = "template.Rmd", 
+    output_file = args[1]),
+  error=function(e) {
+    print(e)
+    quit(status=1) 
+  }
+)
